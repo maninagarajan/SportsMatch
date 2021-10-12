73042946a45d1ee0855273bc0f6f25b99d8983e1
maninagarajan <maninagarajan96@gmail.com>
2021-10-12 10:25:44 +0530

    Remove commented code from the main file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Routing Handler
 
 const routes = {
-  '/': players,
+  '/': login,
   '/login': login,
   '/home': home,
   '/players': players
@@ -26,87 +26,79 @@ window.onpopstate = () => {
 
 // Login Handler
 
-// const loginForm = document.getElementById("login-form");
-// const loginButton = document.getElementById("login-form-submit");
-// const loginErrorMsg = document.getElementById("login-error-msg");
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
-// loginButton.addEventListener("click", (e) => {
-//   e.preventDefault();
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
 
-//   const username = loginForm.username.value;
-//   const password = loginForm.password.value;
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
 
-//   console.log(username, password);
+  console.log(username, password);
 
 
-//   if (username.toLowerCase() === "admin" && password.toLowerCase() === "admin@123") {
-//     // alert("You have successfully logged in.");
-//     loginErrorMsg.style.opacity = 0;
-//     onNavigate('/home');
-//   } else {
-//     loginErrorMsg.style.opacity = 1;
-//     console.log('Error');
-//   }
-// });
+  if (username.toLowerCase() === "admin" && password.toLowerCase() === "admin@123") {
+    // alert("You have successfully logged in.");
+    loginErrorMsg.style.opacity = 0;
+    onNavigate('/home');
+  } else {
+    loginErrorMsg.style.opacity = 1;
+    console.log('Error');
+  }
+});
 
 
 
 // Home screen eventHandler
 
-// const addForm = document.getElementById("player-add-form");
-// const addButton = document.getElementById("player-add-form-submit");
-// const closeModal = document.getElementById("close-modal");
-// const openModal = document.getElementById("add-player");
-// const modalContainer = document.getElementsByClassName("modal-container")[0];
+const addForm = document.getElementById("player-add-form");
+const addButton = document.getElementById("player-add-form-submit");
+const closeModal = document.getElementById("close-modal");
+const openModal = document.getElementById("add-player");
+const modalContainer = document.getElementsByClassName("modal-container")[0];
 
-// document.getElementsByTagName('span')[0].innerHTML = JSON.parse(localStorage.getItem('Players'))?.length || 0;
+document.getElementsByTagName('span')[0].innerHTML = JSON.parse(localStorage.getItem('Players'))?.length || 0;
 
-// addButton.addEventListener("click", (e) => {
-//   e.preventDefault();
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
 
-//   console.log(addForm.playerName.value, addForm.location.value, addForm.sports.value, addForm.playerPicture.value);
+  console.log(addForm.playerName.value, addForm.location.value, addForm.sports.value, addForm.playerPicture.value);
 
-//   let newPlayer = {
-//     name: addForm.playerName.value,
-//     location: addForm.location.value,
-//     sports: addForm.sports.value,
-//     profilePic: addForm.playerPicture.value
-//   };
+  let newPlayer = {
+    name: addForm.playerName.value,
+    location: addForm.location.value,
+    sports: addForm.sports.value,
+    profilePic: addForm.playerPicture.value
+  };
 
-//   let playersList = JSON.parse(localStorage.getItem('Players')) || [];
+  let playersList = JSON.parse(localStorage.getItem('Players')) || [];
 
-//   playersList.push(newPlayer);
-//   console.log(playersList);
-//   localStorage.setItem('Players', JSON.stringify(playersList));
+  playersList.push(newPlayer);
+  console.log(playersList);
+  localStorage.setItem('Players', JSON.stringify(playersList));
 
-//   document.getElementsByTagName('span')[0].innerHTML = playersList.length;
-//   modalContainer.style.display = 'none';
-// });
+  document.getElementsByTagName('span')[0].innerHTML = playersList.length;
+  modalContainer.style.display = 'none';
+});
 
 
-// openModal.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   modalContainer.style.display = 'block';
-// });
+openModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalContainer.style.display = 'block';
+});
 
-// closeModal.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   modalContainer.style.display = 'none';
-// });
+closeModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalContainer.style.display = 'none';
+});
 
 
 
 // Players List screen eventHandler
 
 const savedPlayers = JSON.parse(localStorage.getItem('Players'));
-
-// let newPlayer = {
-//   name: 'Sam',
-//   location: 'Kochi',
-//   sports: 'Sports',
-//   profilePic: '2.png'
-// };
-// localStorage.setItem('Players', JSON.stringify([newPlayer]));
 
 const columnHeader = ['Profile', 'Name','Sports', 'Location'];
 const columnData = ['profilePic','name','sports','location'];
@@ -141,8 +133,6 @@ for (var i = 0; i < savedPlayers.length; i++) {
 // Add the newly created table containing json data
 var el = document.getElementById("playersTable");
 el.innerHTML = "";
-console.log(table);
 el.appendChild(table);
 
-// playersList.innerHTML = ;
 
